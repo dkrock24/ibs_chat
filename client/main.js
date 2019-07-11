31db92f084364864749c9e0cfba356845b2c5eca
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2019-07-11 07:51:51 -0600

    validation message empty

--- client/main.js
+++ client/main.js
@@ -2,13 +2,15 @@ var socket = io.connect('http://172.25.179.49:6677',{'forceNew':true});
 
 var photo ="";
 $.ajax({
-  url: 'https://randomuser.me/api/',
-  dataType: 'json',
-  success: function(data) {
-  	photo = data.results[0].picture.thumbnail;
-    console.log(data.results[0].picture);
-    console.log(photo);
-  }
+
+	url: 'https://randomuser.me/api/',
+	dataType: 'json',
+
+	success: function(data) {
+		photo = data.results[0].picture.thumbnail;
+		console.log(data.results[0].picture);
+		console.log(photo);
+	}
 });
 
 socket.on('messages',function(data){
@@ -30,11 +32,15 @@ function addmessage(e){
 		photo: photo
 	};
 
-	document.getElementById('nickname').style.display = 'none';
-	document.getElementById('text').value = '';
-	document.getElementById('text').focus();
-	socket.emit('add-message', message);
-	return false;
+	if(message.text.length > 0){
+		console.log("A ",message.text);
+		document.getElementById('nickname').style.display = 'none';
+		document.getElementById('text').value = '';
+		document.getElementById('text').focus();
+		socket.emit('add-message', message);
+		return false;
+	}
+	
 }
 
 function render2(data){
@@ -45,30 +51,28 @@ function render2(data){
 			if(message.nickname == nick ){
 
 				return (`
-				<div class="incoming_msg">
-	              <div class="incoming_msg_img"> <img src="${message.photo}" alt="sunil"> </div>
-		              <div class="received_msg">
-		                <div class="received_withd_msg">
-		                  <p>${message.text}</p>
-		                  <span class="time_date"><b>${message.nickname}</b> 11:01 AM    |    June 9</span></div>
-		              </div>
-		            </div>
-	            </div><br>
-			`);
+					<div class="incoming_msg">
+					<div class="incoming_msg_img"> <img src="${message.photo}" alt="sunil"> </div>
+						<div class="received_msg">
+							<div class="received_withd_msg">
+							<p>${message.text}</p>
+							<span class="time_date"><b>${message.nickname}</b> 11:01 AM    |    June 9</span></div>
+						</div>
+						</div>
+					</div><br>
+				`);
 			}else{	
 				return (`
-
 				
-						<div class="outgoing_msg">
-						<div class="sent_msg_img"> <img src="${message.photo}" alt="sunil"> </div>
-			              <div class="sent_msg">
-			                  <p>${message.text}</p>
-			                 <span class="time_date"><b>${message.nickname}</b> 11:01 AM    |    June 9</span></div>
-			            </div>
-			            </div>
-		           
+					<div class="outgoing_msg">
+					<div class="sent_msg_img"> <img src="${message.photo}" alt="sunil"> </div>
+						<div class="sent_msg">
+							<p>${message.text}</p>
+							<span class="time_date"><b>${message.nickname}</b> 11:01 AM    |    June 9</span></div>
+					</div>
+					</div>
 
-					`);
+				`);
 			}
 		}
 	}).join(' ');
